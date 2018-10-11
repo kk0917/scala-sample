1581de5bea7c5df0d2be2920d04dcae25a8ec9dd
ko-kamenashi <ko-kamenashi@dac.co.jp>
2018-10-11 09:22:05 +0900

    test
    * test of git push action without option

--- src/main/scala/MyApp.scala
+++ src/main/scala/MyApp.scala
@@ -26,19 +26,4 @@ trait MyApp extends App {
     targetMessage.headOption.toRight(s"$messageId not found.")
   })
   val messageList2: Seq[Message] = result.collect { case Right(message) => message }
-
-
-  // sample
-  def f(messageId: MessageId): Either[String, Message] = {
-    val targetMessage = MessageService.find(MessageFindForm(messageId = messageId))
-    targetMessage.headOption.toRight(s"$messageId not found.")
-  }
-
-  val result1: Seq[Either[String, Message]] = messageIds.map(f)
-  val errors1: Seq[Either[String, Message]] = result1.filter(_.isLeft)
-  val errors2: Seq[String] = result1.collect({case Left(errorMessage) => errorMessage})
-
-  // List[List[Message]]みたいにネストしている構造を文字通りフラットにしてくれる
-  val result2: List[Message] = messageIds.flatMap(messageId => MessageService.find(MessageFindForm(messageId = messageId)))
-  val result3: List[Message] = messageIds.map(messageId => MessageService.find(MessageFindForm(messageId = messageId))).flatten
 }
